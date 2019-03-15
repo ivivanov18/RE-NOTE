a48a6f7e209a7d3e0ccc32728e772434d20b7ba7
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-03-15 13:41:26 +0200

    feat: add Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <CssBaseline />
+        <Navbar />
       </React.Fragment>
     );
   }
